917a220e24038a293b7783e52f0aa473915f7f4f
jalayna <jalaynatipantiza@gmail.com>
2020-08-01 23:35:38 +0000

    set up rails and react and initial seed

---
+++ atlas_jams_api/db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(  name: "Landon",  email: "example@gmail.com", number: 3064703625, profile_pic: "https://www.mercurynews.com/wp-content/uploads/2020/07/CCT-SKETCHPICKS-0112-01.jpg", description:"Lively band", username: "folks123", city: "saskatoon", is_performer: true, size: 3 )
