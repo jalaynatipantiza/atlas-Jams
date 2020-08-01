81dae534c9d95c345556dc77deb1cde4268191f2
jalayna <jalaynatipantiza@gmail.com>
2020-08-01 23:49:48 +0000

    re-installed react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+import axios from 'axios'
 
-class App extends Component {
-
-  axios.get("http://localhost:3000/users")
-  .then(res => console.log(res.data))
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+function App() {
+  axios.get("http://localhost:3000/")
+    .then(res => console.log(res.data))
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
