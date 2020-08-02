73c897601b3ac9ebd5e55f1d7295cefd17694b69
landon tipantiza <landontipantiza@gmail.com>
2020-08-02 18:45:31 +0000

    added 2 performers and 2 hosts

--- atlas_jams_api/db/seeds.rb
+++ atlas_jams_api/db/seeds.rb
@@ -6,9 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-performer1 = User.findbyname
 
-performer1 = User.create( 
+
+
+
+#users
+
+#perfermers
+performer1 = User.create({ 
   name: "Lando",  
   email: "example@gmail.com", 
   number: 3064703625, 
@@ -18,8 +23,75 @@ performer1 = User.create(
   location: "saskatoon", 
   is_performer: true, 
   size: 4 
-)
+})
+performer2 = User.create({ 
+  name: "Massimo",  
+  email: "massimo123@gmail.com", 
+  number: 3062437814, 
+  profile_pic: "https://images.unsplash.com/photo-1573470167254-315246dd477d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", 
+  description:"I'm a classical piano musician.", 
+  username: "massimo78", 
+  location: "Calgary", 
+  is_performer: true, 
+  size: 1 
+})
 
 
+#hosts
+host1 = User.create({ 
+  name: "Rheema Timmer",  
+  email: "rheemaexample@gmail.com", 
+  number: 3064703625, 
+  profile_pic: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", 
+  description:"i love to host bands at my grape vineyard and help local artists get exposure", 
+  username: "rheemat", 
+  location: "calary", 
+  is_performer: false
+})
 
 
+host2 = User.create({ 
+  name: "Milana Rivera",  
+  email: "milana@gmail.com", 
+  number: 3064703612, 
+  profile_pic: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", 
+  description:"Backyard garden space", 
+  username: "milanagarden", 
+  location: "saskatoon", 
+  is_performer: false, 
+})
+
+
+#genre_category
+
+jazz = GenreCategory.create({
+  name: "Jazz"
+})
+
+folk = GenreCategory.create({
+  name: "Folk"
+})
+
+classical = GenreCategory.create({
+  name: "Classical"
+})
+
+rock = GenreCategory.create({
+  name: "Rock"
+})
+
+pop = GenreCategory.create({
+  name: "Pop"
+})
+
+cultural = GenreCategory.create({
+  name: "Cultural"
+})
+
+
+#genres
+
+genre1 = Genre.create({
+  user_id: 1,
+  genre_categories_id: 1
+})
