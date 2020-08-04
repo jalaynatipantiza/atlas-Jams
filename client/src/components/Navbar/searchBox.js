5c79d5f416c869904f93c1472db798e6466be2d2
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-08-03 20:46:09 -0600

    fix bug in seachbox file

--- client/src/components/Navbar/searchBox.js
+++ client/src/components/Navbar/searchBox.js
@@ -18,13 +18,7 @@ const useStyles = makeStyles({
     height: "100vh",
     width: "100vw"
   },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
+
 });
 const SearchBox = (props) => {
   const classes = useStyles();
