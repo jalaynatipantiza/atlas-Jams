108ae2c6dfc78eafbfc744d5bc6d606efe42d272
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-08-04 13:53:20 -0600

    fix scrollable box

--- client/src/components/PerformerProfile/PerformerProfile.js
+++ client/src/components/PerformerProfile/PerformerProfile.js
@@ -7,6 +7,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import { Box } from '@material-ui/core';
 
 
 const useStyles = makeStyles({
@@ -14,6 +16,11 @@ const useStyles = makeStyles({
   media: {
     height: "60vh",
     width:"45vw"
+  },
+  box: {
+    height: "30vh",
+    width: "45vw",
+    overflow: "auto"
   }
 });
 
@@ -41,11 +48,15 @@ export default function Profile(props) {
           {<br></br>}
           {`Size: ${size}`}
           </Typography>
-          <Typography variant="body2" component="p">
-            {`${description} "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`}
+
+        <Box className={classes.box}>
+          <Typography paragraph>
+          {`${description} "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.sunt in culpa qui officia deserunt mollit anim id est laborum.sunt in culpa qui officia deserunt mollit anim id est laborum."`}
           </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+        </Box>
+
+      </CardContent>
+    </CardActionArea>
+  </Card>
   )
 };
