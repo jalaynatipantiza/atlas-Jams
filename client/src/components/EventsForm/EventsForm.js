e1b4b67ccfdead487b26debb2ec37d8761c02ca3
Joshua <joshuagomberoff@gmail.com>
2020-08-05 14:01:00 -0700

    Center form

--- client/src/components/EventsForm/EventsForm.js
+++ client/src/components/EventsForm/EventsForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,6 +9,21 @@ const useStyles = makeStyles((theme) => ({
       width: '25ch',
     },
   },
+  container: {
+    width: '40%',
+    margin: '100px auto 0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  field: {
+    width: '100%',
+    margin: 10,
+  },
+  nonField: {
+    width: '100%',
+    margin: 20,
+  },
 }));
 
 export default function SignupForm() {
@@ -16,16 +31,27 @@ export default function SignupForm() {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <h2>Event Info</h2>
-      <TextField id="standard-basic" label="Name" />
-      <TextField id="standard-basic" label="Date" />
-      <TextField id="standard-basic" label="Time" />
-      <TextField id="standard-basic" label="Duration" />
-      <TextField id="standard-basic" label="Price" />
-      <TextField id="standard-basic" label="Description" multiline />
-      <TextField id="standard-basic" label="Performer(s)" />
-      <Button variant="contained" color="primary" href="#">Add Performer</Button>
-      <Button variant="contained" color="primary" href="#">Submit</Button>
+      <Grid 
+        container
+        className={classes.container}
+      >
+        <div className={classes.nonField}>
+          <h2>Event Info</h2>
+        </div>
+        <TextField id="standard-basic" label="Name" className={classes.field} />
+        <TextField id="standard-basic" label="Date" className={classes.field} />
+        <TextField id="standard-basic" label="Time" className={classes.field} />
+        <TextField id="standard-basic" label="Duration" className={classes.field} />
+        <TextField id="standard-basic" label="Price" className={classes.field} />
+        <TextField id="standard-basic" label="Description" className={classes.field} multiline />
+        <TextField id="standard-basic" label="Performer(s)" className={classes.field} />
+        <div className={classes.nonField}>
+          <Button variant="contained" color="primary" href="#">Add Performer</Button>
+        </div>
+        <div className={classes.nonField}>
+          <Button variant="contained" color="primary" href="#">Submit</Button>
+        </div>
+      </Grid>
     </form>
   );
 }
