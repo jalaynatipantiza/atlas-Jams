5eac8c635d4867da95fdcbf8d05d368a04a9f987
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-08-06 12:47:21 -0600

    added event button, fix profile picture to be scalable

--- client/src/components/HostProfile/styles/styles.js
+++ client/src/components/HostProfile/styles/styles.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles( (theme) => ({
   mainPic: {
     backgroundImage:  "url('https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')",
-    height: "400px",
-    width: "350px",
+    width: "100%",
+    height: "40%",
     backgroundPosition: "center",
     backgroundSize: "cover"
   },
@@ -35,7 +35,17 @@ const useStyles = makeStyles({
     display: "flex",
     alignItems: "center",
     font: "initial"
+  },
+  buttonEvent: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  addButton: {
+    backgroundColor: "chocolate",
+    color: "white",
+    marginTop: "10px"
   }
-});
+}));
 
 export default useStyles;
