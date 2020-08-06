d22c5c6a49cb20ba240aa394663812542d11adde
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-08-06 23:42:29 +0000

    Capture event state

--- client/src/components/EventsCards/EventsCards.js
+++ client/src/components/EventsCards/EventsCards.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
@@ -19,40 +20,41 @@ const useStyles = makeStyles({
 });
 
 
-export default function EventsCard(props) {
+export default function EventsCard({ description, name, date, event_picture, time, am, id, key, setEvent }) {
 
-  const {description, name, date, event_picture, time, am} = props
 
   const classes = useStyles();
   const partialDescription = description.slice(0, 100);
 
   return (
-    <Card className={classes.root} style={{margin:"10px"}}>
-      <CardActionArea >
-        <CardMedia
-          className={classes.media}
-          image={event_picture}
+    <Link to={`/events/${id}`} style={{ textDecoration: 'none', color: "inherit" }} onClick={() => setEvent(id)}>
+      <Card className={classes.root} style={{margin:"10px"}}>
+        <CardActionArea >
+          <CardMedia
+            className={classes.media}
+            image={event_picture}
 
-        />
-        <CardContent >
-          <Typography gutterBottom variant="h5" component="h1">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          {`${date} ${time}`}
-          { am ? "am" : "pm"}
+          />
+          <CardContent >
+            <Typography gutterBottom variant="h5" component="h1">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {`${date} ${time}`}
+            { am ? "am" : "pm"}
 
-          </Typography>
-          <Typography variant="body2" component="p">
-            {partialDescription}...
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions style={{display: "flex", justifyContent: "center"}} >
-        <Button size="small" color="primary" style={{alignSelf:"center"}}>
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+            </Typography>
+            <Typography variant="body2" component="p">
+              {partialDescription}...
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions style={{display: "flex", justifyContent: "center"}} >
+          <Button size="small" color="primary" style={{alignSelf:"center"}}>
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </Link>
   );
 }
