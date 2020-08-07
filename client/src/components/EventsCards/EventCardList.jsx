0ba427395ca8a713279372e1cd758590d6fd5531
Joshua <joshuagomberoff@gmail.com>
2020-08-07 16:49:33 -0700

    Passing in event capacity and num_of_attendees

--- client/src/components/EventsCards/EventCardList.jsx
+++ client/src/components/EventsCards/EventCardList.jsx
@@ -6,8 +6,9 @@ import useStyles from './styles/styles';
 export default function EventsCardList({ events, sideScroll }) {
   const classes = useStyles();
 
-  const list = events.map((event)=>{
-    return <EventsCards id={event.id} key={event.id} description={event.description} name={event.name} time={event.time} date={event.date} event_picture={event.event_picture} am={event.am} />
+
+  const list = events.map(({event, capacity, num_of_attendees})=>{
+    return <EventsCards id={event.id} key={event.id} description={event.description} name={event.name} time={event.time} date={event.date} event_picture={event.event_picture} am={event.am} capacity={capacity} num_of_attendees={num_of_attendees} />
   })
   return (
     <div className={classes.card} style={sideScroll? {flexWrap:"nowrap"}: null}>
