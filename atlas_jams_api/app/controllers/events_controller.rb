c425ad87d89234cd71bbf86c73582d30a6f7014b
Joshua <joshuagomberoff@gmail.com>
2020-08-07 12:26:27 -0700

    Add my_genres query to performer

--- atlas_jams_api/app/controllers/events_controller.rb
+++ atlas_jams_api/app/controllers/events_controller.rb
@@ -7,25 +7,15 @@ class EventsController < ApplicationController
   def show
     # Grab event based on id
     # render json
-
-    # @events = EventsPerformer.joins(:event).where(user_id: params[:id]).select('*')
-
     @event = Event.find_by id: params[:id]
-
-    # render json: @events, @performers, @host
 
     @space = Space.find_by id: @event.space_id
 
     @host = User.find_by id: @space.user_id
 
-    # @events_performers = EventsPerformer.find_by event_id: @event.id
+    @performers = EventsPerformer.joins(:user).where(event_id: @event.id).select('*').as_json(methods: [:my_genres])
 
-    @performers = EventsPerformer.joins(:user).where(event_id: @event.id).select('*')
-
-    # render json: @performers
     render json: { 'event' => @event, 'performers' => @performers, 'space' => @space, 'host' => @host }
-
-    # render json: @events
   end
 
   def userEventsPerformer
