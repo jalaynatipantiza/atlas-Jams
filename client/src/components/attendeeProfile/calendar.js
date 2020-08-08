4ac3ed150113e949f80dd54b10592e49e1cf2d9a
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-08-08 17:06:21 -0600

    positioned calendar and added clickable event

--- client/src/components/attendeeProfile/calendar.js
+++ client/src/components/attendeeProfile/calendar.js
@@ -4,18 +4,14 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction";
 
 export default function EventCalendar (){
-  
-  const handleDateClick = (arg) => { 
-    alert(arg.dateStr)
-  }
+
     return (
-     <FullCalendar 
+     <FullCalendar
      defaultView="dayGridMonth"
      plugins={[ dayGridPlugin]}
      events={[{title: "Upcoming Event", date: "2020-08-22"}, {title: " Upcoming Event", date: "2020-08-23"}]}
-
+     eventColor={'#FFA07A'}
+     eventClick={() => {alert("Testing")}}
      />
     )
-  
-  
 };
