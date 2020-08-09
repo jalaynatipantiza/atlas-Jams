4e0494f758b4eebffcb2e9185a754d84e6a97252
landon tipantiza <landontipantiza@gmail.com>
2020-08-09 23:48:00 +0000

    fixed profile page bug, removed ticket count from event creation form

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -104,7 +104,7 @@ const Navbar = (props) => {
             open={Boolean(anchorEl)}
             onClose={handleClose}
           >
-      { loggedIn && <Link to={`${userType}/${loggedIn}`} style={{ textDecoration: 'none', color:"black" }}>
+      { loggedIn && <Link to={`/${userType}/${loggedIn}`} style={{ textDecoration: 'none', color:"black" }}>
             <MenuItem onClick={handleClose}>Profile</MenuItem>
           </Link>}
            { !loggedIn && <Link to={`/login`} style={{ textDecoration: 'none', color:"black" }}>
