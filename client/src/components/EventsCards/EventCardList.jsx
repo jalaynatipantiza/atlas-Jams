7ce2e6a14a4fc3ce478325fe7eea041e7272efb7
jalayna <jalaynatipantiza@gmail.com>
2020-08-09 00:31:13 +0000

    made events on calendar dynamic for attendee

--- client/src/components/EventsCards/EventCardList.jsx
+++ client/src/components/EventsCards/EventCardList.jsx
@@ -8,7 +8,6 @@ import { useState } from 'react';
 export default function EventsCardList({ events, sideScroll, cardDeleted }) {
   const classes = useStyles();
 
-  console.log("it is what it is",events);
   const list = events.map((event)=>{
     return <EventsCards id={event.id} key={event.id} description={event.description} name={event.name} time={event.time} date={event.date} event_picture={event.event_picture} am={event.am} accepted={event.accepted} user_id={event.user_id} location={event.location} cardDeleted={cardDeleted} />
   })
